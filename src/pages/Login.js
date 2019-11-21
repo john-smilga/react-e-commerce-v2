537dc598f7bfa446b469d80227222c10a746c8ea
John <johnsmilga@gmail.com>
2019-11-21 05:22:51 -0800

    form structure added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,43 @@
 import React from "react";
 
 export default function Login() {
-  return <h1>hello from login page</h1>;
+  return (
+    <section className="form">
+      <h2 className="section-title">sign in</h2>
+      <form className="login-form">
+        {/* single input */}
+        <div className="form-control">
+          <label for="email">email</label>
+          <input type="email" id="email" />
+        </div>
+        {/*end of single input */}
+        {/* single input */}
+
+        <div className="form-control">
+          <label for="password">password</label>
+          <input type="password" id="password" />
+        </div>
+        {/*end of single input */}
+
+        {/* single input */}
+
+        <div className="form-control">
+          <label for="username">username</label>
+          <input type="text" id="username" />
+        </div>
+        {/*end of single input */}
+        {/* empty form text */}
+        <p className="form-empty">please fill out all form fields</p>
+        {/* submit btn */}
+        <button type="submit" className="btn btn-block btn-primary">
+          submit
+        </button>
+        {/* register link */}
+        <p className="register-link">
+          need to register?
+          <button type="button">click here</button>
+        </p>
+      </form>
+    </section>
+  );
 }
